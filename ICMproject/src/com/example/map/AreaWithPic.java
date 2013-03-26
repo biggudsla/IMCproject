f89ae70bab014ce9bf02a0d097e78276ac152ebc
moonkim <moonkim@192.168.0.6>
2013-03-26 14:43:44 +0900

    with 맵뷰 업뎃 구 별로 사진 나

--- ICMproject/src/com/example/map/AreaWithPic.java
+++ ICMproject/src/com/example/map/AreaWithPic.java
@@ -34,9 +34,14 @@ public class AreaWithPic {
 		ctrCode = _ad.getCountryCode(); // kr
 		ctrName = _ad.getCountryName(); // 대한민
 		
+		local = _ad.getSubLocality(); // 마포구, 구 단위 존재하면 구 단위로 분
+		if(local == null)		
+			local = _ad.getLocality(); //군포시
 		
+/*		
 		local = _ad.getLocality(); // 군포시 
 		if(local == null)
 			local = _ad.getSubAdminArea(); //노원구 
+			*/
 	}
 }
